2ae440e06586b8659915f9524bc6a00bcd8bd436
Tobias Klug <tobias.klug@codecentric.de>
2026-09-07 14:48:26 +0200

    Change pins to match current breadboard setup

--- src/firmware/platform/rp2040/board/pico_wh.zig
+++ src/firmware/platform/rp2040/board/pico_wh.zig
@@ -3,10 +3,10 @@ const rp2xxx = @import("microzig").hal;
 pub const pin_config = rp2xxx.pins.GlobalConfiguration{
     .GPIO0 = .{ .name = "oled_sda", .direction = .in, .pull = .up },
     .GPIO1 = .{ .name = "oled_scl", .direction = .in, .pull = .up },
-    .GPIO22 = .{ .name = "temp", .direction = .in, .pull = .up },
+    .GPIO16 = .{ .name = "temp", .direction = .in, .pull = .up },
     .GPIO21 = .{ .name = "heater", .direction = .out, .pull = .down },
-    .GPIO20 = .{ .name = "ultra_sound_trigger", .direction = .out, .pull = .down },
-    .GPIO19 = .{ .name = "ultra_sound_echo", .direction = .in, .pull = .down },
+    .GPIO10 = .{ .name = "ultra_sound_trigger", .direction = .out, .pull = .down },
+    .GPIO11 = .{ .name = "ultra_sound_echo", .direction = .in, .pull = .down },
     // Baker's physical on/off toggle switch. Wired to ground, so pull-up
     // reads .low on the pin when the switch is on.
     .GPIO18 = .{ .name = "power_switch", .direction = .in, .pull = .up },
